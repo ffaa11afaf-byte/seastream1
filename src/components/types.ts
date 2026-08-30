0632026e9317f43278b15fe536922500108c82c9
ffaa11afaf-byte <ffaa11afaf@gmail.com>
2026-08-30 05:30:30 +0300

    Add files via upload

---
+++ src/components/types.ts
@@ -0,0 +1,21 @@
+export interface FeaturedAnime {
+  title: string;
+  description: string;
+  poster: string;
+  rating: string;
+}
+
+export interface Episode {
+  number: string;
+  title: string;
+  anime: string;
+  poster: string;
+  rating: string;
+}
+
+export interface PopularAnime {
+  title: string;
+  genre: string;
+  poster: string;
+  rating: string;
+}
